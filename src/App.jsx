5e0783279b3ef139c0352249be60b97c27ab4574
Shailu <shailesh.respond@gmail.com>
2023-12-20 20:41:21 +0530

    Added logout auto logout features and also addressing security issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { Routes, Route } from "react-router-dom";
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
 import Layout from "./components/Layout/Layout";
 import UserProfile from "./components/Profile/UserProfile";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="/auth" element={<AuthPage />}></Route>
-        <Route path="/profile" element={<UserProfile />}></Route>
+        <Route
+          path="/auth"
+          element={!authCtx.isLoggedIn ? <AuthPage /> : <Navigate to="/" />}
+        ></Route>
+        <Route
+          path="/profile"
+          element={authCtx.isLoggedIn ? <UserProfile /> : <Navigate to="/" />}
+        ></Route>
+        <Route path="*" element={<Navigate to="/" />}></Route>
       </Routes>
     </Layout>
   );
--- src/store/auth-context.jsx
+++ src/store/auth-context.jsx
@@ -6,24 +6,34 @@ const AuthContext = React.createContext({
   isLoggedIn: false,
   login: (token) => {},
   logout: () => {},
+  autologout: () => {},
 });
 
 //Named export
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const initilaToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initilaToken);
   const userIsLoggedIn = !!token;
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem("token", token);
+    autologoutHandler();
   };
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
   };
-
+  const autologoutHandler = () => {
+    setTimeout(() => {
+      logoutHandler();
+    }, 50000);
+  };
   const contextValue = {
     token: token,
     isLoggedIn: userIsLoggedIn,
     login: loginHandler,
     logout: logoutHandler,
+    autologout: autologoutHandler,
   };
 
   return (
